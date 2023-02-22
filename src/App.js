c37811df5f637dc01e4341ac7105ec6644b373c6
Jodie Drover <jodiedrover@live.com>
2023-02-22 14:42:16 -0500

    Capture values from Form in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, React } from "react";
 import './App.css';
 import LoginButton from 'components/LoginButton';
 import LogoutButton from 'components/LogoutButton';
@@ -5,13 +6,27 @@ import Profile from 'components/Profile';
 import Form from 'components/Form';
 
 function App() {
+  const [pets, setPets] = useState([]);
+
+  /**
+   *
+   * @param { Object } pet: An object of objects containing values for new pet profiles
+   * Object { value: }
+   * Values: name, fixed, breed, sex, age, location, description, size, housetrained
+   */
+  function addPet(pet) {
+    setPets([...pets, pet]);
+  }
+
+  console.log(pets);
+
   return (
     <div className="App">
 
     <LoginButton/>
     <LogoutButton/>
     <Profile/>
-    <Form/>
+    <Form addPet={addPet}/>
     </div>
 
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -37,7 +37,7 @@ export default function Form(props) {
       description
     }
 
-    // props.[Function(newProfile)] ==> Use this to ... thread? This information needs to be passed to PetListItem?
+    props.addPet(newProfile);
     // console.log(newProfile)
   }
 
@@ -46,44 +46,62 @@ export default function Form(props) {
     <form className="new-profile" onSubmit={onSubmit} autoComplete="off">
 
       <div className="text-input">
-    <label htmlFor="name">Name*</label><br/>
+    <label htmlFor="name">
+      Name* <br/>
       <input {...name} id="name" placeholder="Enter your pet's name" required /><br/><br/>
+      </label>
 
-    <label htmlFor="breed">Breed</label><br/>
+    <label htmlFor="breed">
+      Breed <br/>
       <input {...breed} id="breed" placeholder="Enter your pet's breed" /><br/><br/>
+      </label>
 
-    <label htmlFor="age">Age</label><br />
+    <label htmlFor="age">
+      Age <br />
       <input {...age} id="age" placeholder="Enter your pet's age" /><br/><br/>
+      </label>
       </div>
 
 <div className="radios">
-    <label htmlFor="sex">Sex</label><br />
+    <label htmlFor="sex">
+      Sex <br />
       <input {...sex} value="Male" type="radio" name="sex" /> Male<br/>
       <input {...sex} value="Female" type="radio" name="sex" /> Female<br/><br/>
+      </label>
 
-    <label htmlFor="fixed">Spayed/Neutered*</label><br />
+    <label htmlFor="fixed">
+      Spayed/Neutered* <br />
       <input {...fixed} value="Yes" type="radio" name="fixed" required /> Yes<br/>
       <input {...fixed} value="No" type="radio" name="fixed" /> No<br/><br/>
+      </label>
 
-    <label htmlFor="trained">Housetrained*</label><br />
+    <label htmlFor="trained">
+      Housetrained* <br />
       <input {...housetrained} value="Yes" type="radio" name="trained" required /> Yes<br/>
       <input {...housetrained} value="No" type="radio" name="trained" /> No<br/><br/>
+    </label>
 
-    <label htmlFor="size">Size</label><br />
-      <input {...size} value="xs (2 - 12 lbs)" type="radio" name="size" /> xs (2 - 12 lbs)<br/>
+    <label htmlFor="size">
+      Size* <br />
+      <input {...size} value="xs (2 - 12 lbs)" type="radio" name="size" required /> xs (2 - 12 lbs)<br/>
       <input {...size} value="s (13 - 24 lbs)" type="radio" name="size" /> s (13 - 24 lbs)<br/>
       <input {...size} value="m (25 - 59 lbs)" type="radio" name="size" /> m (25 - 59 lbs)<br/>
       <input {...size} value="L (60 - 99 lbs)" type="radio" name="size" /> L (60 - 99 lbs)<br/>
       <input {...size} value="XL (over 100 lbs)" type="radio" name="size" /> XL ( > 100 lbs)<br/><br/>
+    </label>
       </div>
 
       <div className="text-input">
-    <label htmlFor="location">Location*</label><br />
+    <label htmlFor="location">
+      Location* <br />
       <input {...location} id="location" placeholder="City, Province" required /><br/><br/>
+    </label>
       </div>
 
-    <label htmlFor="description">Description</label><br />
+    <label htmlFor="description">
+    Description <br />
       <input {...description} id="description" placeholder="How would your pet describe themselves?" /><br/>
+      </label>
 
       <button type="submit">Sign up your pet!</button>
     </form>
