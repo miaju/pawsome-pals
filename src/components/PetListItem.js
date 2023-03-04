85dc7f2ad9c1d88da73bbc6e2df68454ff6a53c6
Edlyn Shih <edlyn0418@gmail.com>
2023-03-03 22:45:31 -0500

    Add link to individual pet page in petlistitem

--- src/components/PetListItem.js
+++ src/components/PetListItem.js
@@ -1,12 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import "./styling/PetListItem.scss"
 
 export default function PetListItem(props) {
   return (
     <div className="pet-card">
+      <Link to={`/pets/${props.id}`} state={{data: props}} >
       <div className="pet-image">
         <img className="image" src={props.photo_url} alt={props.name} />
       </div>
+      </Link>
       <div className="pet-info">
         <p className="bg-light border">{props.name}</p>
       </div>
