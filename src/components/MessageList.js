1da077e4c5cad3ef586e6ab61f560647c2808fac
Jodie Drover <jodiedrover@live.com>
2023-03-06 00:01:02 -0500

    Draft styling for MessageListItem

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,22 +1,26 @@
-import { useState, React } from "react";
+import { React } from "react";
+import MessageListItem from "components/MessageListItem";
 
 // SMS messages can only be sent after a match has been made
 // Pictures:on hover -- "SEE PROFILE" -> href on click to profile
-export default function MessageList()
- {
+/**
+ *
+ * @param {Object} props: { userId: integer, name: string, message: string, photo_url: string }
+ * @returns
+ */
+export default function MessageList(props) {
   return (
     <div>
-      Pawtential Matches
       <header>Sent SMS Messages</header>
       <div id="messages-container">
         <div className="messages">
-          Picture Here: Sent message here. <br />
-          Picture Here: Sent message here. <br />
-          Picture Here: Sent message here. <br />
-          Picture Here: Sent message here. <br />
+          {/* to be replaced with map of messageListItem */}
+          <MessageListItem />
+          <MessageListItem />
+          <MessageListItem />
+          <MessageListItem />
         </div>
-
       </div>
     </div>
   );
- }
+}
