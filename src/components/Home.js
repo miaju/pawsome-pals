49d76c4d20c7dce144a8327371cb5b5b9862db94
Edlyn Shih <edlyn0418@gmail.com>
2023-03-06 13:03:01 -0500

    add banner and css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import banner from "./styling/homepage-banner.jpg"
 
 import "./styling/Home.scss";
 
@@ -13,18 +14,24 @@ const Home = (props) => {
     ) : user ?
     <>
     <div className="home">
-      <h1>Welcome {user.name}!</h1>
-      <p>
-        Why don't you check out the <a href="/explore"> explore page</a>?
-      </p>
+      <img className="home-banner" src={banner} alt="banner"/>
+      <div className="banner-text">
+        <h1>Welcome {user.name}!</h1>
+        <p>
+          Why don't you check out the <a href="/explore"> explore page</a>?
+        </p>
+      </div>
     </div>
     </> :
     <>
       <div className="home">
-        <h1>Welcome to Pawsome Pals!</h1>
-        <p>Here you can make friends for life for your pet!</p>
-        <p>Please log in to get started.</p>
-        <Button size="lg" onClick={loginWithRedirect}>Log in</Button>
+        <img className="home-banner" src={banner} alt="banner"/>
+        <div className="logout-text">
+          <h1>Welcome to Pawsome Pals!</h1>
+          <p>Here you can make friends for life for your pet!</p>
+          <p>Please log in to get started.</p>
+          <Button size="lg" onClick={loginWithRedirect}>Log in</Button>
+        </div>
       </div>
     </> )
 
