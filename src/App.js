8f68212bc7d5912aa0994bd39a74ffac6c74572b
Jodie Drover <jodiedrover@live.com>
2023-03-06 22:24:38 -0500

    Correct user_id used in axios post to pets api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, React, useEffect } from "react";
-import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -13,9 +13,8 @@ import Advanced from "components/MatchListTest";
 import shuffle from "components/helpers/shuffleArray";
 import MatchList from "components/MatchList";
 import MatchDetail from "components/MatchDetail";
-import MessageList from "components/MessageList";
+// import MessageList from "components/MessageList";
 import Footer from "components/Footer";
-
 
 function App() {
   const [allpets, setAllpets] = useState([]);
@@ -29,127 +28,177 @@ function App() {
   const [matches, setMatches] = useState([]);
   const [users, setUsers] = useState([]);
   const [checked, setChecked] = useState(false);
-  const { user, loginWithRedirect, logout, isLoading, isAuthenticated } = useAuth0();
-
+  const [userId, setUserId] = useState(null);
+  const { user, loginWithRedirect, logout, isLoading, isAuthenticated } =
+    useAuth0();
 
   const handlePetChange = (id) => {
-    localStorage.setItem('currentpet', JSON.stringify(id));
-    setCurrentpet(JSON.parse(localStorage.getItem('currentpet')));
+    localStorage.setItem("currentpet", JSON.stringify(id));
+    setCurrentpet(JSON.parse(localStorage.getItem("currentpet")));
   };
 
   async function getUserByEmail(email) {
     setChecked(true);
     return await axios
-      .get(`http://localhost:8080/api/users`, {params: {'email': email}} )
-      .catch(err => console.error(err));
+      .get(`http://localhost:8080/api/users`, { params: { email: email } })
+      .catch((err) => console.error(err));
   }
-
 
   useEffect(() => {
     const getData = (userId) => {
-      axios.get("http://localhost:8080/api/pets")
-      .then((response) => {
-        const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
+      axios.get("http://localhost:8080/api/pets").then((response) => {
+        const data = Object.entries(response.data).map(([key, value]) => ({
+          ...value,
+        }));
         setAllpets(shuffle(data));
       });
-      axios.get(`http://localhost:8080/api/pets/${userId}`)
-      .then((response) => {
-        const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
+      axios.get(`http://localhost:8080/api/pets/${userId}`).then((response) => {
+        const data = Object.entries(response.data).map(([key, value]) => ({
+          ...value,
+        }));
         setPets(shuffle(data));
       });
-      axios.get("http://localhost:8080/api/users")
-      .then((response) => {
-        const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
+      axios.get("http://localhost:8080/api/users").then((response) => {
+        const data = Object.entries(response.data).map(([key, value]) => ({
+          ...value,
+        }));
         setUsers(data);
       });
-    }
+    };
     if (user) {
-       getUserByEmail(user.name).then(res => {
-        const userId = (Object.keys(res.data)[0]);
+      getUserByEmail(user.name).then((res) => {
+        const userId = Object.keys(res.data)[0];
         getData(userId);
-       });
+      });
     }
   }, [user]);
 
-
   useEffect(() => {
-    const currentId = JSON.parse(localStorage.getItem('currentpet'));
+    const currentId = JSON.parse(localStorage.getItem("currentpet"));
     if (currentId) {
-          console.log(currentId);
-          axios.get(`http://localhost:8080/api/matches/${currentId}`)
-          .then((response) => {
-            const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
-            setMatches(data);
-          });
-        }
-      }, [currentpet])
-
+      console.log(currentId);
+      axios
+        .get(`http://localhost:8080/api/matches/${currentId}`)
+        .then((response) => {
+          const data = Object.entries(response.data).map(([key, value]) => ({
+            ...value,
+          }));
+          setMatches(data);
+        });
+    }
+  }, [currentpet]);
 
   useEffect(() => {
     if (user && !checked) {
-
       async function addUser(user) {
         return await axios
-          .post('http://localhost:8080/api/users', {'email': user.name})
-          .then(response => {
+          .post("http://localhost:8080/api/users", { email: user.name })
+          .then((response) => {
             setUsers([...users, user]);
           });
       }
 
       getUserByEmail(user.name)
-      .then(response => {
-        if(Object.keys(response.data).length === 0) {
-          addUser(user);
-        }
-      }).catch(err => console.log(err));
-
-    }
-  }, [checked, user, users])
-
-  /**
-   *
-   * @param { Object } pet: An object of objects containing values for new pet profiles
-   * Values: name, breed, age, sex, size, spayed_or_neutered, city, description, photo_url
-   */
-   async function addPet(pet) {
-      const { name, breed, age, sex, size, spayed_or_neutered, city, description, photo_url } = pet;
-
-      return axios
-        .post(`http://localhost:8080/api/pets`, {
-          'name': name,
-          'breed': breed,
-          'age': age,
-          'sex': sex,
-          'spayed_or_neutered': spayed_or_neutered,
-          'size': size,
-          'city': city,
-          'description': description,
-          'photo_url': photo_url
+        .then((response) => {
+          if (Object.keys(response.data).length === 0) {
+            addUser(user);
+          }
         })
-        .then((res) => {
-          console.log("made it here")
-          setPets([...pets, pet]);
-          return redirect("http://localhost:3000/pets/view");
-        });
+        .catch((err) => console.log(err));
     }
+  }, [checked, user, users]);
 
 
+
+  useEffect(() => {
+    if (user) {
+      getUserByEmail(user.name).then((res) => {
+        const userId = Object.keys(res.data)[0];
+        setUserId(userId);
+      });
+    }
+  }, [user]);
+
+  async function addPet(pet) {
+    const currentId = userId;
+
+    const {
+      name,
+      breed,
+      age,
+      sex,
+      size,
+      spayed_or_neutered,
+      city,
+      description,
+      photo_url,
+    } = pet;
+
+    return axios
+      .post(`http://localhost:8080/api/pets`, {
+        user_id: currentId,
+        name: name,
+        breed: breed,
+        age: age,
+        sex: sex,
+        spayed_or_neutered: spayed_or_neutered,
+        size: size,
+        city: city,
+        description: description,
+        photo_url: photo_url,
+      })
+      .then((res) => {
+        console.log("made it here");
+        setPets([...pets, pet]);
+        // return redirect("http://localhost:3000/pets/view");
+      });
+  }
 
   return (
     <div>
       <BrowserRouter>
-        <NavBar user={user} isLoading={isLoading} loginWithRedirect={loginWithRedirect} logout={logout}></NavBar>
+        <NavBar
+          user={user}
+          isLoading={isLoading}
+          loginWithRedirect={loginWithRedirect}
+          logout={logout}
+        ></NavBar>
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home user={user} isLoading={isLoading} logout={logout} loginWithRedirect={loginWithRedirect}/>} />
+            <Route
+              path="/"
+              element={
+                <Home
+                  user={user}
+                  isLoading={isLoading}
+                  logout={logout}
+                  loginWithRedirect={loginWithRedirect}
+                />
+              }
+            />
             <Route path="/pets/new" element={<Form addPet={addPet} />} />
-            <Route path="/pets/view" element={<PetList pets={pets} current={currentpet} />} />
-            <Route path="/pets/:id" element={<PetDetail handlePetChange={handlePetChange} />} />
-            <Route path="/profile" element={<Profile user={user} logout={logout} isAuthenticated={isAuthenticated}/>} />
-            <Route path="/explore" element={<Advanced pets={allpets} />}/>
+            <Route
+              path="/pets/view"
+              element={<PetList pets={pets} current={currentpet} />}
+            />
+            <Route
+              path="/pets/:id"
+              element={<PetDetail handlePetChange={handlePetChange} />}
+            />
+            <Route
+              path="/profile"
+              element={
+                <Profile
+                  user={user}
+                  logout={logout}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
+            />
+            <Route path="/explore" element={<Advanced pets={allpets} />} />
             <Route path="/matches" element={<MatchList matches={matches} />} />
             <Route path="/matches/:id" element={<MatchDetail />} />
-            <Route path="/messages" element={<MessageList />} />
+            {/* <Route path="/messages" element={<MessageList />} /> */}
           </Routes>
         </div>
       </BrowserRouter>
