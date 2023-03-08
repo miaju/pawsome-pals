de40018a7ab9dde6384ee738d8a2de686069c657
Mia Tanner <mia@indygecko.com>
2023-03-08 12:14:38 -0800

    add tabs for different types of matches

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -5,11 +5,11 @@ import "./styling/MatchList.scss"
 import MatchListItem from "./MatchListItem";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFaceSadTear } from '@fortawesome/free-solid-svg-icons'
+import Nav from "react-bootstrap/Nav";
 
 export default function MatchList(props) {
   let [empty, setEmpty] = useState(false);
-
-  let {pending, matchee, matches} = props;
+  let [content, setContent] = useState({matches: props.matches, title: 'Matches'});
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,145 +19,46 @@ export default function MatchList(props) {
 
 
   return (
-    (matches && pending && matches) ? (
-      <section className="matches-container">
-        {matches.length ? <h1>Matches</h1> : <></>}
-        {(matches.length) ? (
-          matches.map(match => (
-           <MatchListItem
-             key={match.id}
-             id={match.id}
-             name={match.name}
-             breed={match.breed}
-             age={match.age}
-             sex={match.sex}
-             size={match.size}
-             spayed_or_neutered={match.spayed_or_neutered}
-             city={match.city}
-             description={match.description}
-             photo_url={match.photo_url}
-             current={props.current}
-          />
-           )
-         )
-        ) : (<></>)}
-        {pending.length ? <h1>Pending Matches</h1> : <></>}
-        {(pending.length) ? (
-          pending.map(match => (
-           <MatchListItem
-             key={match.id}
-             id={match.id}
-             name={match.name}
-             breed={match.breed}
-             age={match.age}
-             sex={match.sex}
-             size={match.size}
-             spayed_or_neutered={match.spayed_or_neutered}
-             city={match.city}
-             description={match.description}
-             photo_url={match.photo_url}
-             current={props.current}
-          />
-           )
-         )
-        ) : (<></>)}
-        {matchee.length ? <h1>Checkout who matched with You</h1> : <></>}
-        {(matchee.length) ? (
-          matchee.map(match => (
-           <MatchListItem
-             key={match.id}
-             id={match.id}
-             name={match.name}
-             breed={match.breed}
-             age={match.age}
-             sex={match.sex}
-             size={match.size}
-             spayed_or_neutered={match.spayed_or_neutered}
-             city={match.city}
-             description={match.description}
-             photo_url={match.photo_url}
-             current={props.current}
-          />
-           )
-         )
-        ) : (<></>)}
-        { (empty && !matches.length && !matchee.length && !pending.length) &&
-          <div className="empty-card">
-            <h2 className="message">No existing match found for the current pet <FontAwesomeIcon icon={faFaceSadTear} /></h2>
-          </div>
-        }
-      </section>
+    (props.matches && props.pending && props.matchee) ? (
+      <>
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link onClick={() => setContent({matches: props.matches, title: 'Matches'})}>Matches</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => setContent({matches: props.pending, title: 'Pending'})}>Pending</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={() => setContent({matches: props.matchee, title: 'Matched with'})} >
+            Matchee
+          </Nav.Link>
+        </Nav.Item>
+      </Nav><section className="matches-container">
+          {content.matches.length ? <h1>{content.title}</h1> : <></>}
+          {(content.matches.length) ? (
+            content.matches.map(match => (
+              <MatchListItem
+                key={match.id}
+                id={match.id}
+                name={match.name}
+                breed={match.breed}
+                age={match.age}
+                sex={match.sex}
+                size={match.size}
+                spayed_or_neutered={match.spayed_or_neutered}
+                city={match.city}
+                description={match.description}
+                photo_url={match.photo_url}
+                current={props.current} />
+            )
+            )
+          ) : (<></>)}
+          {(empty && !content.matches.length) &&
+            <div className="empty-card">
+              <h2 className="message">No existing {content.title.toLowerCase()} found for the current pet <FontAwesomeIcon icon={faFaceSadTear} /></h2>
+            </div>}
+        </section>
+        </>
     ) : (<></>)
   );
-
-  // return ( (matches && pending && matches) ?
-  //   <section className="matches-container">
-  //     {props.matches.length > 0 &&
-  //       props.matches.map(match => (
-  //         <MatchListItem
-  //           key={match.id}
-  //           id={match.id}
-  //           name={match.name}
-  //           breed={match.breed}
-  //           age={match.age}
-  //           sex={match.sex}
-  //           size={match.size}
-  //           spayed_or_neutered={match.spayed_or_neutered}
-  //           city={match.city}
-  //           description={match.description}
-  //           photo_url={match.photo_url}
-  //           current={props.current}
-  //         />
-  //         )
-  //       )
-  //     }
-  //     <h1>Pending Matches</h1>
-  //     {props.pending.length > 0 &&
-  //       props.pending.map(match => (
-  //         <MatchListItem
-  //           key={match.id}
-  //           id={match.id}
-  //           name={match.name}
-  //           breed={match.breed}
-  //           age={match.age}
-  //           sex={match.sex}
-  //           size={match.size}
-  //           spayed_or_neutered={match.spayed_or_neutered}
-  //           city={match.city}
-  //           description={match.description}
-  //           photo_url={match.photo_url}
-  //           current={props.current}
-  //         />
-  //         )
-  //       )
-  //     }
-  //     <h1>Check out who matched with You</h1>
-  //     {props.matchee.length > 0 &&
-  //       props.matchee.map(match => (
-  //         <MatchListItem
-  //           key={match.id}
-  //           id={match.id}
-  //           name={match.name}
-  //           breed={match.breed}
-  //           age={match.age}
-  //           sex={match.sex}
-  //           size={match.size}
-  //           spayed_or_neutered={match.spayed_or_neutered}
-  //           city={match.city}
-  //           description={match.description}
-  //           photo_url={match.photo_url}
-  //           current={props.current}
-  //         />
-  //         )
-  //       )
-  //     }
-  //     { (!matches.length && !matchee.length && !pending.length) &&
-
-  //       <div className="empty-card">
-  //         <h2 className="message">No existing match found for the current pet <FontAwesomeIcon icon={faFaceSadTear} /></h2>
-  //       </div>
-
-  //     }
-  //   </section> : <></>
-  // )
 }
