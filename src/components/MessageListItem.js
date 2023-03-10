d22b463bdb19a7f72911a2a569c73a918aade39f
Edlyn Shih <edlyn0418@gmail.com>
2023-03-09 23:06:00 -0500

    add styling to messages

--- src/components/MessageListItem.js
+++ src/components/MessageListItem.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./styling/MessageListItem.scss";
 
 export default function MessageListItem(props) {
-  console.log('HELLLLLLLLLO', props)
+  console.log('Message list item PROPS', props)
   return (
-    <div className="messages">
-      <img src={props.from_pet_url} alt={props.from_pet_name}/>
-      <div className="message-details">
+    <div className="chat">
+      <img className="chat-image" src={props.from_pet_url} alt={props.from_pet_name}/>
+      <div className="chat-details">
         <h2>{props.from_pet_name}</h2>
         <p>{props.message}</p>
       </div>
-      <p className="message-timestamp">{props.timestamp}</p>
+      <p className="chat-timestamp">{props.timestamp}</p>
     </div>
   );
 }
