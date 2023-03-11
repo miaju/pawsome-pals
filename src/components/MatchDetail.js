0599dcb94b4423c64b917e64fc9fa712885e64a7
Mia Tanner <mia@indygecko.com>
2023-03-10 17:24:34 -0800

    add confirm popup for deleting match

--- src/components/MatchDetail.js
+++ src/components/MatchDetail.js
@@ -1,27 +1,39 @@
-import React from "react"
+import React, {useState} from "react"
 import "./styling/MatchItem.scss"
 import { useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faHeartCrack, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import UnMatchPopup from "./UnMatchPopup";
 
 export default function MatchDetail(props) {
 
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const unMatch = () => {
+    setLoading(true)
+    props.unmatch(props.currentId, location.state.data.id);
+  }
+
   const location = useLocation();
   const navigate = useNavigate();
+  console.log(props)
+  console.log(location);
 
   let email = props.getUserByPet(location.state.data.id).then(e => {
     email = e.data.email;
   });
 
   return (
-    <div style={{height: "80vh"}}>
-      <section class="match-card">
-        <div class="right">
-          <img class="image" src={location.state.data.photo_url} alt={props.name} />
+    <><UnMatchPopup setShow={setShow} show={show} unMatch={unMatch} loading={loading} />
+    <div style={{ height: "80vh" }}>
+      <section className="match-card">
+        <div className="right">
+          <img className="image" src={location.state.data.photo_url} alt={props.name} />
         </div>
-        <div class="left">
-          <div class="match-name">{location.state.data.name}</div>
-          <div class="match-info">
+        <div className="left">
+          <div className="match-name">{location.state.data.name}</div>
+          <div className="match-info">
             Breed: {location.state.data.breed}<br />
             Age: {location.state.data.age}<br />
             Sex: {location.state.data.sex}<br />
@@ -29,13 +41,13 @@ export default function MatchDetail(props) {
             City: {location.state.data.city}<br />
             Description: {location.state.data.description}
           </div>
-          <div class="match-buttons">
+          <div className="match-buttons">
             <button
-              onClick={() => { props.unmatch(props.currentId, location.state.data.id) }} >
+              onClick={() => setShow(true)}>
               Unmatch <FontAwesomeIcon icon={faHeartCrack} />
             </button>
             <button
-              onClick={() => window.location.href = `mailto:${email}`} >
+              onClick={() => window.location.href = `mailto:${email}`}>
               Message <FontAwesomeIcon icon={faEnvelope} />
             </button>
           </div>
@@ -44,6 +56,6 @@ export default function MatchDetail(props) {
       <div>
         <button className="back-btn" onClick={() => navigate(-1)}><FontAwesomeIcon icon={faArrowLeft} /> Back</button>
       </div>
-    </div>
+    </div></>
   )
 }
