980b3898a167764ed0c4d3ddd388382ca32f9268
Edlyn Shih <edlyn0418@gmail.com>
2023-03-13 00:02:27 -0400

    make message detail styling page and import to message detail

--- src/components/MessageDetail.js
+++ src/components/MessageDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from 'axios';
+import "./styling/MessageDetail";
 
 export default function MessageDetail(props) {
   const location = useLocation();
