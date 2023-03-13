587787ad3ef25eb40cb821b097098ddb2f8c7587
Edlyn Shih <edlyn0418@gmail.com>
2023-03-13 01:39:26 -0400

    styling and hide footer in message detail page

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
   const [checked, setChecked] = useState(false);
   const [userId, setUserId] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [showFooter, setShowFooter] = useState(true);
   const { user, loginWithRedirect, logout, isLoading, isAuthenticated } =
     useAuth0();
 
@@ -297,9 +298,9 @@ function App() {
                 />
               }
             />
- 
+
             <Route path="/messages" element={<MessageList currentId={currentpet.id} messages={uniqueMessages} />} />
-            <Route path="/messages/:id" element={<MessageDetail messages={messages} />} />
+            <Route path="/messages/:id" element={<MessageDetail messages={messages} setShowFooter={setShowFooter} />} />
             <Route
               path="/matches/:id"
               element={<MatchDetail unmatch={unmatch} currentId={currentpet.id} getUserByPet={getUserByPet} addMatch={addMatch}/>} />
@@ -330,7 +331,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
-      <Footer />
+      {showFooter && <Footer />}
     </div>
   );
 }
--- src/components/MessageDetail.js
+++ src/components/MessageDetail.js
@@ -7,46 +7,53 @@ import "./styling/MessageDetail.scss";
 export default function MessageDetail(props) {
   const location = useLocation();
   console.log('LOCATION>>>>>>>', location.state.data)
-console.log('Message detail PROPS', props)
+  console.log('Message detail PROPS', props)
 
 
-const [privateMsgs, setPrivateMsgs] = useState([]);
+  const [privateMsgs, setPrivateMsgs] = useState([]);
 
-useEffect(() => {
-  if (location.state.data.from_petId && location.state.data.to_petId) {
-    axios.get(`http://localhost:8080/api/messages/chat/${location.state.data.from_petId}/${location.state.data.to_petId}`)
-    .then((response) => {
-      const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
-      console.log(data.length);
-      setPrivateMsgs(data);
-  })}
-}, [location.state.data.from_petId, location.state.data.to_petId])
-console.log('chat', privateMsgs)
+  useEffect(() => {
+    if (location.state.data.from_petId && location.state.data.to_petId) {
+      axios.get(`http://localhost:8080/api/messages/chat/${location.state.data.from_petId}/${location.state.data.to_petId}`)
+        .then((response) => {
+          const data = Object.entries(response.data).map(([key, value]) => ({ ...value }))
+          console.log(data.length);
+          setPrivateMsgs(data);
+        })
+    }
+  }, [location.state.data.from_petId, location.state.data.to_petId])
+  console.log('chat', privateMsgs)
 
   return (
     <div className="chatScreen">
       {privateMsgs.map((message) => (
         location.state.data.currentpet !== message.from_petid ? (
-        <div className="chatScreen_message">
-          <Avatar
-            className="chatScreen_image"
-            alt={message.from_pet_name}
-            src={message.from_pet_photo_url}
-            />
-          <p className="chatScreen_text">{message.message}</p>
-        </div>
-        ) :
-        (
           <div className="chatScreen_message">
-            <p className="chatScreen_textUser">{message.message}</p>
             <Avatar
-            className="chatScreen_image"
-            alt={message.from_pet_name}
-            src={message.from_pet_photo_url}
+              className="chatScreen_image"
+              alt={message.from_pet_name}
+              src={message.from_pet_photo_url}
             />
+            <p className="chatScreen_text">{message.message}</p>
           </div>
-        )
+        ) :
+          (
+            <div className="chatScreen_message">
+              <p className="chatScreen_textUser">{message.message}</p>
+              <Avatar
+                className="chatScreen_image"
+                alt={message.from_pet_name}
+                src={message.from_pet_photo_url}
+              />
+            </div>
+          )
       ))}
+
+      <form className="chatScreen_input">
+        <input className="chatScreen_inputField" placeholder="Type a message..." type="text" />
+        <button className="chatScreen_inputButton">SEND</button>
+      </form>
+      {props.setShowFooter(false)}
     </div>
   )
 }
